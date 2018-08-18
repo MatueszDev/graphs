62f36997d7c77e915fd3f9a42688ee6c14f6faf5
MatueszDev <m.seweryn96@gmail.com>
2018-08-18 23:09:44 +0200

    graphic timeToEachNode visualisation

--- main.cpp
+++ main.cpp
@@ -2,15 +2,14 @@
 
 int main()
 {
-    MixGraph mt22("net22g4.dat");
+    MixGraph mt22("net22g5.dat");
+    //MixGraph mt22(2,2);
 	//mt22.printNetwork(1);
-	std::vector<std::vector<double>> data = mt22.calculateTimeFromEachNodToHub(10);
+	std::vector<std::vector<double>> data = mt22.calculateTimeFromEachNodToHub(50);
 	mt22.exportRandomWalkResultToDataFile(data);
 	//mt22.createHistogramFile();
-	// mt22.nextGenerationV2();
-	// mt22.nextGenerationV2();
-    // mt22.nextGenerationV2();
-    // mt22.nextGenerationV2();
+	// for(int i=0; i < 5;i++)
+    //     mt22.nextGenerationV2();
 	// mt22.exportNetworkToFile();
 
 
--- nodeTohub.py
+++ nodeTohub.py
@@ -1,6 +1,7 @@
 import glob
-import pprint
+from pprint import pprint as pp
 import matplotlib.pyplot as plt
+import numpy as np
 
 networks = {}
 
@@ -12,8 +13,18 @@ for file in glob.glob('randomWalkDat/*.dat'):
         hub = data.readline().split()[2]
         networks.setdefault(uniqueNetworkId,{})
         networks[uniqueNetworkId].setdefault(hub,{})
-        print(generation,u,v,uniqueNetworkId,hub)
         for line in data.readlines():
             degree, time = line.split()
-            networks[uniqueNetworkId][hub].setdefault(degree,[]).append(float(time))
-pprint.pprint(networks)
+            networks[uniqueNetworkId][hub].setdefault(int(degree),[]).append(float(time))
+
+plt.style.use('bmh')
+for net in networks.values():
+    for hub in net.keys():
+        for degree, values in net[hub].items():
+            plt.plot([degree for i in values], values, '.')
+        plt.xlabel("Degree of node")
+        plt.ylabel("Time to hub")
+        plt.title("Random walk from each node to hub {} of {}".format(int(hub) + 1, len(net)))
+        plt.plot(list(net[hub].keys()), [np.average(values) for values in net[hub].values()])
+        plt.show()
+        break

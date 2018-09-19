5d768a7a3e778e0aa143a3e0c34d5e54d4032132
MatueszDev <m.seweryn96@gmail.com>
2018-09-19 17:50:54 +0200

    additional parameter

--- nodeTohub.py
+++ nodeTohub.py
@@ -5,29 +5,30 @@ import numpy as np
 from collections import OrderedDict
 
 networks = {}
-
+prob = {}
 for file in glob.glob('randomWalkDat/*.dat'):
     with open(file) as data:
         generation = data.readline()[:-1]
         u,v = data.readline().split()
         uniqueNetworkId = data.readline()[:-1]
-        probability = data.readline()[:-1]
-        hub = data.readline().split()[2]
         networks.setdefault(uniqueNetworkId,{})
+        probability = data.readline()[:-1]
+        prob.setdefault(uniqueNetworkId, probability)
+        hub = data.readline().split()[2]
         networks[uniqueNetworkId].setdefault(hub,{})
         for line in data.readlines():
             degree, time = line.split()
             networks[uniqueNetworkId][hub].setdefault(int(degree),[]).append(float(time))
 
 plt.style.use('bmh')
-for net in networks.values():
+for name,net in networks.items():
     for hub in net.keys():
         ordered = OrderedDict(sorted(net[hub].items()))
         for degree, values in ordered.items():
             plt.plot([degree for i in values], values, '.')
         plt.xlabel("Degree of node")
         plt.ylabel("Time to hub")
-        plt.title("Random walk from each node to hub {} of {} with {} tree".format(int(hub) + 1, len(net), probability))
+        plt.title("Random walk from each node to hub {} of {} with {} tree".format(int(hub) + 1, len(net), prob[name]))
         plt.plot(list(ordered.keys()), [np.average(values) for values in ordered.values()])
-        plt.show()
+        plt.savefig("toPack/net22_g5p{}_{}.pdf".format(prob[name],int(hub) + 1 ))
         plt.clf()
